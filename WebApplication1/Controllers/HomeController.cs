a17a4a0c147a7aba27b9db8ec64683747c69d80e
Ashutosh Yaduwanshi <ashutosh.chipsoft@gmail.com>
2021-09-27 22:00:02 +0530

    Add project files.

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ProcessStartInfo processStartInfo = new ProcessStartInfo();
+            return View();
+        }
+
+        public ActionResult Python()
+        {
+            return View();
+        }
+
+        public ActionResult MediaPipe()
+        {
+            return View();
+        }
+
+        public ActionResult Test()
+        {
+            return View();
+        }
+    }
+}
